66f4d9af2970fcc2ecc8ae5307a3c2b6cdfb66be
elyor <elyor3600610@gmail.com>
2022-08-12 22:07:40 +0500

    initial commit

---
+++ app/src/main/java/com/example/food_delivery/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.food_delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_delivery.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        binding.text.text = "Hello 915-19";
+    }
+}
